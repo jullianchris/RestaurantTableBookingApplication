6120c4d3d2d7a3083fd97f82e0ae457f8b0d4afb
jullianchris <jullianchristopher6@gmail.com>
2024-11-14 23:01:51 +0530

    Add project files.

---
+++ RTBApp.Core/Reservation.cs
@@ -0,0 +1,20 @@
+﻿namespace RTBApp.Core
+{
+    public partial class Reservation
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int TimeSlotId { get; set; }
+
+        public DateTime ReservationDate { get; set; }
+
+        public string ReservationStatus { get; set; } = null!;
+
+        public virtual TimeSlot TimeSlot { get; set; } = null!;
+
+        public virtual User User { get; set; } = null!;
+        public bool ReminderSent { get; set; } = false;
+    }
+}
